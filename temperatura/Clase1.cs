3152d8386619b0a48629d8d280c87ceb44755d37
Cupsuki001 <124387400+Cupsuki001@users.noreply.github.com>
2024-02-01 11:26:09 -0600

    Add files via upload

---
+++ temperatura/Clase1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace temperatura
+{
+    internal class Clase1
+    {
+        private double[] celc = new double[10];
+
+        public Clase1() { }
+        public Clase1(double[] Celc)
+        {
+            celc = Celc;
+        }
+        public void conver(double[] Celc)
+        {
+            this.celc = Celc;
+        }
+
+        public void setcelc(double [] celc) 
+        {
+          this.celc=celc;
+        }
+
+        public int validar(String Mensaje)
+        {
+            int num = 0, v;
+            do
+            {
+                v = 1;
+                try
+                {
+                    Console.Write(Mensaje);
+                    num = Convert.ToInt32(Console.ReadLine());
+                }
+                catch (Exception error)
+                {
+                    v = 0;
+                    Console.WriteLine("No se aceptan letras", error.Message);
+                }
+            } while (v == 0);
+            return num;
+        }
+    }
+}
---
+++ temperatura/Program.cs
@@ -0,0 +1,55 @@
+﻿// See https://aka.ms/new-console-template for more information
+using temperatura;
+
+public class temp
+{
+    public static void Main(string[] args)
+    {
+        Clase1 c = new Clase1();
+        double[] cc = new double[10];
+        double t , sal;
+        double [] f = new double [10];
+        double[] k = new double[10];
+
+        do
+        {
+
+
+            do
+            {
+                t = c.validar("Digite la cantidad de temperatura celsius:  ");
+
+
+            } while (t <= 0 || t > 10);
+
+            for (int i = 0; i < t; i++)
+            {
+                do
+                {
+                    cc[i] = c.validar("Celsius " + (1 + i) + ":");
+                    c.setcelc(cc);
+
+                } while (cc[i] <= -273.15 || cc[i] > 100);
+
+                f[i] = (cc[i] * 9 / 5) + 32;
+                k[i]= cc[i] + 273.15;
+            }
+            Console.WriteLine("-----------------------------------");
+            for (int i = 0; i < t; i++) 
+            {
+                Console.WriteLine("Celsius:" + cc[i] + " Farenheit: " + f[i] + " Kelvin: " + k[i]);
+                Console.WriteLine("------------------------");
+
+            }
+
+            do
+            {
+                sal = c.validar("Desea seguir en el programa? 1.Si 2.No" + "\t");
+
+            } while (sal <= 0 || sal > 2);
+
+
+
+        } while (sal==1);
+    }
+}
